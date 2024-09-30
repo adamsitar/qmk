4d19c6c5903bc5e83e61593b922efc6e01e3fd0c
adamsitar <adam.sitar1@gmail.com>
2024-09-30 22:57:41 +0200

    removed annoying combos, added semicolon, switched quote with semicolon

--- keyboards/lily58/keymaps/sitar_lily58_keymap/keymap.c
+++ keyboards/lily58/keymaps/sitar_lily58_keymap/keymap.c
@@ -64,13 +64,13 @@ const uint16_t PROGMEM lbrc_combo[] = {KC_C, KC_D, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(tab_combo, KC_TAB),
-    COMBO(backspace_combo, KC_BSPC),
+    //COMBO(backspace_combo, KC_BSPC),
     // COMBO(c_backspace_combo, LCTL(KC_BSPC)),
     // COMBO(esc_combo, KC_ESC),
 
     COMBO(exclamation_combo, KC_EXLM),
     // COMBO(question_combo, KC_QUES),
-    COMBO(colon_combo, KC_COLN),
+    //COMBO(colon_combo, KC_COLN),
     COMBO(underscore_combo, KC_UNDS),
     COMBO(pipe_combo, KC_PIPE),
 
@@ -86,13 +86,12 @@ combo_t key_combos[] = {
 };
 
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_ZRO] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_GRV,
-  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,  KC_MINS,
-  KC_LSFT,  KC_A,    HOME_R,  HOME_S,  HOME_T,    KC_G,          KC_M,    HOME_N,  HOME_E,  HOME_I,  KC_O,     KC_QUOT,
+  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,  KC_QUOT,
+  OSM(MOD_LSFT),    KC_A,    HOME_R,  HOME_S,  HOME_T,    KC_G,          KC_M,    HOME_N,  HOME_E,  HOME_I,  KC_O,     KC_SCLN,
   KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,  KC_LCBR,  KC_RCBR, KC_K,    KC_H,    TD(SCLN),KC_DOT,  KC_SLSH,  KC_RSFT,
 
             KC_LALT, KC_DEL, OSL(_DW),   KC_SPC,                   KC_ENT, OSL(_UP), KC_BSPC,  KC_RGUI
